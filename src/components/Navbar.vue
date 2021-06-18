187dd5635f6b15fd15ac12bc50c06246a3c987a8
Tauseef Ahmad <tauseef.n89@gmail.com>
2021-06-18 19:27:44 +0530

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>BasicsAdda Blog</h1>
+    <nav>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'Create' }">Create Post</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+  header h1 {
+    color: blueviolet;
+    font-size: 48px;
+  }
+  header a {
+    color: blueviolet;
+    text-decoration: none;
+    margin-left: 20px;
+  }
+  header a.router-link-active {
+    color: black;
+    font-weight: bold;
+  }
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="post">
+    <router-link :to="{ name: 'Details', params: { id: post.id }}">
+      <h3>{{ post.title }}</h3>
+    </router-link>
+    <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">
+      #{{ tag }}
+    </span>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: ['post'],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + '...'
+    })
+
+    return { snippet }
+  }
+}
+</script>
+
+<style>
+  .post {
+    margin: 0 40px 30px;
+    padding-bottom: 30px;
+    border-bottom: 1px dashed #e7e7e7;
+  }
+  .post h3 {
+    display: inline-block;
+    font-size: 26px;
+    color: blueviolet;
+    margin-bottom: 10px;
+    max-width: 400px;
+  }
+</style>
